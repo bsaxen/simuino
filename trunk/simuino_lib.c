7fec5ced851c15299fd7b9f1cccd0622db3aead9
benny.saxen <benny.saxen@ba6b2cdc-f064-1a82-1866-45cc734bcd73>
2011-12-14 20:47:38 +0000

    version 0.1.4

--- trunk/simuino_lib.c
+++ trunk/simuino_lib.c
@@ -78,9 +78,9 @@ void show(WINDOW *win)
     {
       wmove(win,0,2);
       if(confBoardType ==UNO)
-	wprintw(win,"SIMUINO - Arduino UNO Pin Analyzer 0.1.3");
+	wprintw(win,"SIMUINO - Arduino UNO Pin Analyzer 0.1.4");
       if(confBoardType ==MEGA)
-	wprintw(win,"SIMUINO - Arduino MEGA Pin Analyzer 0.1.3");
+	wprintw(win,"SIMUINO - Arduino MEGA Pin Analyzer 0.1.4");
       wmove(win,1,2);
       wprintw(uno,"Sketch: %s",appName);
     }
