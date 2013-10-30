6c25de19168eb158d513892d1c42d0b618f83300
benny.saxen <benny.saxen@ba6b2cdc-f064-1a82-1866-45cc734bcd73>
2013-10-30 21:12:53 +0000

    sketch updated

--- sketchbook/blink.ino
+++ sketchbook/blink.ino
@@ -1,4 +1,49 @@
+// ===============================================
+// SIMUINO_BEGIN
+// ===============================================
+// BOARD_TYPE:  UNO
+// SKETCH_NAME: blink.ino
+// SIM_LENGTH:  600
+// WIN_LAYOUT:    2
+// SO_DELAY:     40
+//================================================
+//  Scenario
+//================================================
+//
+// SCENDIGPIN 10    1    0
+// SCENDIGPIN 10   50    1
+// SCENDIGPIN 10  100    0
+// SCENDIGPIN 10  200    1
+// SCENDIGPIN  9    1    0
+// SCENDIGPIN  9   40    1
+// SCENDIGPIN  9  130    0
+//
+// SCENANAPIN  4    1    5
+// SCENANAPIN  5    1    8 
+// SCENANAPIN  4   80   12
+// SCENANAPIN  5  120   18 
+//
+//================================================
+// Simuino log text customization
+//================================================
+// PINMODE_OUT: 11  "PIN: Led Urgent"
+// PINMODE_OUT: 13  "PIN: Led Blink"
 
+// DIGITALWRITE_LOW:  11  "Waiting"
+// DIGITALWRITE_HIGH: 11  "Urgent"
+// DIGITALWRITE_LOW:  13  "Led is off"
+// DIGITALWRITE_HIGH: 13  "Led is on"
+
+// DIGITALREAD:  9  "Read from nine"
+// DIGITALREAD: 10  "Read from ten"
+
+
+// ANALOGREAD: 4  "read analog four"
+// ANALOGREAD: 5  "read analog five"
+
+// ===============================================
+// SIMUINO_END
+// ===============================================
 /*
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
